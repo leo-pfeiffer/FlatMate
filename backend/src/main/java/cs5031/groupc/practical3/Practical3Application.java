3a16e71a88827e4eedfb9cc4d30dfa3cdcb1f3d7
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-03-23 15:35:35 +0000

    build: setup gradle project

---
+++ backend/src/main/java/cs5031/groupc/practical3/Practical3Application.java
@@ -0,0 +1,13 @@
+package cs5031.groupc.practical3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practical3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practical3Application.class, args);
+	}
+
+}
