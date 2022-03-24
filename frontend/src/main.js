acde38e1641f48ed51d24e9915702e9ea2b58ba8
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-03-24 21:25:51 +0000

    build: properly add font awesome dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,16 @@ import store from "./store";
 import axios from "axios";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 axios.defaults.withCredentials = true;
 
 // axios.defaults.baseURL = "http://localhost:8081/api/"; // todo
 axios.defaults.baseURL = "https://gabbyblog.herokuapp.com/";
 
-axios.interceptors.response.use(undefined, function(error) {
+axios.interceptors.response.use(undefined, function (error) {
   if (error) {
     const originalRequest = error.config;
     if (error.response.status === 401 && !originalRequest._retry) {
@@ -22,12 +25,28 @@ axios.interceptors.response.use(undefined, function(error) {
   }
 });
 
-Vue.use(Buefy);
+library.add(fas);
+Vue.component("vue-fontawesome", FontAwesomeIcon);
+
+Vue.use(Buefy, {
+  defaultIconComponent: "vue-fontawesome",
+  defaultIconPack: "fas",
+  customIconPacks: {
+    fas: {
+      sizes: {
+        "is-small": "1x",
+        "is-medium": "2x",
+        "is-large": "3x",
+      },
+      iconPrefix: "",
+    },
+  },
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
