ef5477b26d3923be616f7bcdd7094688f1f0872a
lukas.scheibl <lukas.scheibl@fit.fraunhofer.de>
2022-03-25 12:00:49 +0000

    added authentication

--- backend/src/main/java/cs5031/groupc/practical3/Practical3Application.java
+++ backend/src/main/java/cs5031/groupc/practical3/Practical3Application.java
@@ -1,7 +1,7 @@
 package cs5031.groupc.practical3;
 
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- backend/src/main/java/cs5031/groupc/practical3/Server.java
+++ backend/src/main/java/cs5031/groupc/practical3/Server.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @SpringBootApplication
@@ -25,5 +29,37 @@ public class Server {
     public String apiDescription() {
         ApiDescriber apiD = new ApiDescriber();
         return apiD.describe();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/api/test")
+    public String apitest() {
+       return "in api test";
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/api/test2")
+    public String apitest2() {
+        return "in api test2";
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/createUser")
+    public String blub() {
+        ApiDescriber apiD = new ApiDescriber();
+        return apiD.describe();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/api/user")
+    public ResponseEntity validateUsername(@RequestParam String user) {
+        boolean validate = false;
+        //TODO: validation logic
+        if (validate){
+            return ResponseEntity.ok().build();
+        }
+        throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "entity not found"
+        );
     }
 }
