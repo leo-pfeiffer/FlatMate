9a60b08f2b37d9b495086738a8b5619083b1403c
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-03-28 11:05:14 +0100

    fix: added noargsconstructor to models

--- backend/src/main/java/cs5031/groupc/practical3/model/Group.java
+++ backend/src/main/java/cs5031/groupc/practical3/model/Group.java
@@ -2,11 +2,13 @@ package cs5031.groupc.practical3.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Group {
     private Long groupId;
     private String name;
