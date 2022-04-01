2ba0fae8c2cf5452d3d29c5c4d2528005b75cd64
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-04-01 12:16:57 +0100

    feat: implement create group logic in lobby

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+const axios = require("axios");
 
 const register = function (data) {
   return axios.post("api/user/create", data);
@@ -15,12 +15,24 @@ const login = function (data) {
   return axios(config);
 };
 
-const logout = function() {
+const logout = function () {
   return axios("logout");
-}
+};
+
+const getCurrentUser = function () {
+  return axios.get("api/user");
+};
+
+const createGroup = async function (groupName) {
+  return await axios.post("api/group/create", null, {
+    params: { groupname: groupName },
+  });
+};
 
 module.exports = {
-  register,
-  login,
-  logout,
+  register: register,
+  login: login,
+  logout: logout,
+  getCurrentUser: getCurrentUser,
+  createGroup: createGroup,
 };
