01086c803107dd0c381e92d258cabd15960d993b
Wygyfop <jonathan.quickfall@gmail.com>
2022-04-04 21:01:36 +0100

    feat: added user creation to login for encryption

--- cli/login.sh
+++ cli/login.sh
@@ -1,17 +1,25 @@
 login() {
   password=$(node ./encrypt.js $2 Zy2GokAWHf5VTA81PKPF)
-
+  if [ ${create} = "true" ]
+  then
+    curl -i \
+    --request POST 'http://localhost:8080/api/user/create' \
+    --header "Content-type: application/json" \
+    --data  "{\"username\":\"$1\", \"password\":\"${password}\"}"
+  fi
   curl -i \
   -c ./SessionID.txt \
   --location --request POST 'http://localhost:8080/login' \
   --form username="$1" \
   --form password="${password}"
 }
-while getopts u:p: flag
+create="false"
+while getopts u:p:c flag
 do
     case "${flag}" in
       u) username=${OPTARG};;
       p) password=${OPTARG};;
+      c) create="true";;
     esac
 done
 
