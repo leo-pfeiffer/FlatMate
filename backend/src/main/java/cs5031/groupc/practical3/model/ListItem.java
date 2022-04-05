7b289f1ae1a8cfe6263670598f5a9b598ef18bd8
lukas.scheibl <lukas.scheibl@fit.fraunhofer.de>
2022-04-05 16:00:15 +0100

    updated style for ListItem

--- backend/src/main/java/cs5031/groupc/practical3/model/ListItem.java
+++ backend/src/main/java/cs5031/groupc/practical3/model/ListItem.java
@@ -10,10 +10,25 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ListItem implements DataProtection {
+
+    /**
+     * The id of the listItem.
+     */
     private Long listItemId;
+
+    /**
+     * The name of the listItem.
+     */
     private String name;
+
+    /**
+     * the list the listItem belongs to.
+     */
     private List list;
 
+    /**
+     * Protects sensible data.
+     */
     @Override
     public void protect() {
         this.list.protect();
